9e498a2a826415c04b72c4ef50861527613b472e
Ryan Stouffer2 <ryry2nd@gmail.com>
2022-06-23 14:41:21 -0400

    saves the data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from waitress import serve
-import socket
+import socket, json
 
 app = Flask(__name__)
 
@@ -9,13 +9,35 @@ s.connect(("8.8.8.8", 80))
 IP = s.getsockname()[0]
 s.close()
 
+data_file = open("data.json")
+data = json.load(data_file)
+
 HOST = '0.0.0.0'
 PORT = 80
 THREADS = 6
 
-@app.route('/')
+numDoors = data["Doors"]
+numWheals = data["Wheals"]
+
+@app.route('/data')
+def data():
+    return render_template("data.html", doors=numDoors, wheals=numWheals)
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        global numDoors, numWheals
+        if request.form['d/w'] == "Doors":
+            numDoors += 1
+        elif request.form['d/w'] == "Wheals":
+            numWheals += 1
+
+        with open('data.json', 'w') as f:
+            json.dump({"Doors": numDoors, "Wheals": numWheals}, f)
+
+        return redirect('/data')
+    else:
+        return render_template("index.html")
 
 if __name__ == '__main__':
     print(f"connecting with ip: {IP} and port: {PORT}")
