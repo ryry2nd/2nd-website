35f72fa91dae787ddd6a5e3fcabecf1816ec38a6
Ryan Stouffer2 <ryry2nd@gmail.com>
2022-06-27 17:56:08 -0400

    saves the name

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from waitress import serve
-import socket, json
+from pysondb import PysonDB
+import socket, html
 
 app = Flask(__name__)
 
@@ -9,38 +10,51 @@ s.connect(("8.8.8.8", 80))
 IP = s.getsockname()[0]
 s.close()
 
-try:
-    data = json.load(open("data.json"))
-except FileNotFoundError:
-    data = {"Doors": 0, "Wheals": 0}
-    json.dump(data, open("data.json", "w"))
+db = PysonDB("data.json")
 
 HOST = '0.0.0.0'
 PORT = 80
 THREADS = 6
 
-numDoors = data["Doors"]
-numWheals = data["Wheals"]
+def isDoors(data):
+    if data['d/w'] == "Doors":
+        return True
+
+def isWheals(data):
+    if data['d/w'] == "Wheals":
+        return True
+
+numDoors = len(db.get_by_query(query=isDoors))
+numWheals = len(db.get_by_query(query=isWheals))
 
 @app.route('/data')
 def data():
-    return render_template("data.html", doors=numDoors, wheals=numWheals)
+    return render_template("data.html", data=db.get_all())
+
+@app.route('/results')
+def results():
+    return render_template("results.html", doors=numDoors, wheals=numWheals)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         global numDoors, numWheals
-        if not request.form:
+        if not 'd/w' in request.form:
             return redirect("/")
-        if request.form['d/w'] == "Doors":
+        elif request.form['d/w'] == "Doors":
             numDoors += 1
         elif request.form['d/w'] == "Wheals":
             numWheals += 1
+        else:
+            return redirect("/")
+        
+        db.add({
+            "name": html.escape(request.form['name']),
+            "d/w": request.form['d/w']
+        })
 
-        with open('data.json', 'w') as f:
-            json.dump({"Doors": numDoors, "Wheals": numWheals}, f)
+        return redirect('/results')
 
-        return redirect('/data')
     else:
         return render_template("index.html")
 
