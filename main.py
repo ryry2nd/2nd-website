8894d806d94797b22746e8e2562eafdbf1843b6c
Ryan Stouffer2 <ryry2nd@gmail.com>
2022-06-27 20:05:42 -0400

    remove debug line

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from waitress import serve
 from pysondb import PysonDB
 import socket, html
