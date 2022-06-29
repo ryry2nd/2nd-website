06a978a1774cdfdedae37a1ff11cd1514854cb56
Ryan Stouffer2 <ryry2nd@gmail.com>
2022-06-29 14:37:13 -0400

    error checking and formating

--- Assets/cubeEquation.py
+++ Assets/cubeEquation.py
@@ -11,10 +11,14 @@ class CubeEquation:
         @app.route("/cubeEquation", methods=["GET", "POST"])
         def cubeEquation():
             if request.method == "POST":
-                n = int(request.form["row"])
-                intAns = int((0.5*(n-abs(n-2))+1)*n**2 + (n-2)*(n**2-(n-2)**2))
-                equation = f"parts=(0.5(({n})-|({n})-2|)+1)({n})^2+(({n})-2)(({n})^2-(({n})-2)^2)"
-                ans = f"parts={intAns}"
+                try:
+                    n = eval(request.form["row"])
+                    intAns = int((0.5*(n-abs(n-2))+1)*n**2 + (n-2)*(n**2-(n-2)**2))
+                    equation = f"parts=(0.5(({n:,g})-|({n:,g})-2|)+1)({n:,g})^2+(({n:,g})-2)(({n:,g})^2-(({n:,g})-2)^2)"
+                    ans = f"parts={intAns:,g}"
+                except (NameError, SyntaxError, ValueError):
+                    equation = ""
+                    ans = ""
             else:
                 equation = ""
                 ans = ""
