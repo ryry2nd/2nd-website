77404653c033172067e386f8979fc5b1e1955173
Ryan Stouffer2 <ryry2nd@gmail.com>
2024-01-09 12:56:09 -0500

    login

--- website/forms.py
+++ website/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 
-class SetUsername(forms.Form):
-    rowNumber = forms.IntegerField(max_length=100)
-
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput, max_length=100)
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['rowNumber'].widget.attrs['autocomplete'] = 'off'
+        self.fields['username'].widget.attrs['autocomplete'] = 'off'
+        self.fields['password'].widget.attrs['autocomplete'] = 'off'
--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
+from . import views, viewsAuth
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('cubeEquation', views.cubeEquation, name='cubeEquation'),
     path('login', views.login, name='login'),
+    path('welcome', viewsAuth.welcome, name='welcome'),
+    path('logout', viewsAuth.log_out, name='logout'),
 ]
