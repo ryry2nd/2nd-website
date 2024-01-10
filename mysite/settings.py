978254881a72e2d6f3f21b085f97188c9a1ceb93
Ryan Stouffer2 <ryry2nd@gmail.com>
2024-01-10 10:53:03 -0500

    fixed virtual enviornment

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 from pathlib import Path
 
