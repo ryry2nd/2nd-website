97043aecb9c75dd06a50beb33891336a8b36a730
Ryan Stouffer2 <ryry2nd@gmail.com>
2024-01-10 12:37:26 -0500

    basic file downloader

--- website/urls.py
+++ website/urls.py
@@ -5,9 +5,10 @@ from . import views, viewsAuth
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin', admin.site.urls),
-    path('cubeEquation', views.cubeEquation, name='cubeEquation'),
-    path('login', views.log_in, name='login'),
-    path('welcome', viewsAuth.welcome, name='welcome'),
-    path('logout', viewsAuth.log_out, name='logout'),
-    path('fileShare', viewsAuth.fileShare, name='fileShare')
+    path('cubeEquation', views.cubeEquation),
+    path('login', views.log_in),
+    path('welcome', viewsAuth.welcome),
+    path('logout', viewsAuth.log_out),
+    path('fileShare', viewsAuth.fileShare),
+    path('download/<path>/', viewsAuth.download),
 ]
--- website/viewsAuth.py
+++ website/viewsAuth.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.shortcuts import render
 from django.contrib.auth import logout
+import os
 
 def welcome(request):
     if not request.user.is_authenticated:
@@ -21,4 +22,16 @@ def fileShare(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect("/")
     
-    return render(request, "fileShare.html")
+    return render(request, "fileShare.html", {"files": os.listdir("downloadFiles")})
+
+def download(request, path):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect("/")
+    
+    file_path = os.path.join("downloadFiles", path)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
