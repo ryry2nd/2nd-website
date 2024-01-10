7f5d528aa90cb18e7af0e0a50044f236a8a59090
Ryan Stouffer2 <ryry2nd@gmail.com>
2024-01-10 16:07:54 -0500

    reused the old main file

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+from mysite.wsgi import application as app
 from waitress import serve
-    
-from mysite.wsgi import application
-    
+import socket
+
+HOST = '0.0.0.0'
+PORT = 80
+THREADS = 6
+
+try:
+    prefix = open("rootName.txt").read()
+except FileNotFoundError:
+    prefix = ""
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+IP = s.getsockname()[0]
+s.close()
+
 if __name__ == '__main__':
-    serve(application, port='8000')
+    print(f"connecting with ip: {IP} and port: {PORT}")
+    serve(app, host=HOST, port=PORT, threads=THREADS)
